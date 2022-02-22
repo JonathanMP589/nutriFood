4c7a1c8a18faeaed58a7aa467a76a831fa1345de
jonathan <jonathan.mendiola@softhealth.com.mx>
2022-02-22 17:02:44 -0600

    cambios en diseño

--- src/components/recopilacionDatos/step2.jsx
+++ src/components/recopilacionDatos/step2.jsx
@@ -32,7 +32,8 @@ export const Step2 = () => {
     const { control, formState: { errors }, handleSubmit, } = useForm();
 
     const getFormErrorMessage = (name) => {
-        return errors[name] && <small className='p-error mb-3'>{errors[name].message}</small>
+        console.log(errors);
+        return errors[name] && <small className='p-error mb-1'>{errors[name].message}</small>
     };
 
     return (
@@ -46,31 +47,35 @@ export const Step2 = () => {
 
             <div className='flex'>
                 <img src="/assets/iniciar_sesion_logo.png" alt="Logo de iniciar sesión" className='w-2 absolute left-0 m-4' />
-                <div className=' flex flex-wrap md:justify-content-evenly justify-content-center align-items-center row md:mt-3 mt-7 md:w-11 w-12'>
+                <div className=' flex flex-wrap md:justify-content-evenly justify-content-center align-items-center row md:w-11 w-12 h-screen'>
                     <img className='w-4' src="/assets/Datos_titulo_1.png" alt="Datos" />
-                    <Card className=' md:w-6 w-11' style={{ borderRadius: '5%' }} >
-                        <h2 className='text-center' style={{ color: "#43b06f" }}>Pregunta 1/2</h2>
-                        <form className="flex flex-column p-3 sm:col-12" onSubmit={handleSubmit(goToStep3DataCollection)}>
-                            <Controller name='edad' control={control} rules={{ required: 'Los nombres son requeridos' }}
+                    <div className='w-7 h-full' style={{
+                        background: 'url("/assets/Datos_mano.png")',
+                        backgroundRepeat: 'no-repeat',
+                        backgroundSize: 'cover'
+                    }}>
+                        <form className="flex flex-column p-3 sm:col-2 absolute" style={{ right: "36%" }} onSubmit={handleSubmit(goToStep3DataCollection)}>
+                            <h2 className='text-center mt-5' style={{ color: "#43b06f" }}>Pregunta 1/2</h2>
+                            <Controller name='edad' control={control} rules={{ required: true }}
                                 render={({ field, fieldState }) => (
-                                    <InputNumber className={fieldState.invalid ? 'md-5 p-invalid' : 'mb-5 md-5'}
+                                    <InputNumber className={fieldState.invalid ? 'md-5 p-invalid mb-2' : 'mb-2 md-5'}
                                         placeholder='Edad' value={field.value} onValueChange={(e) => field.onChange(e.value)} suffix=" años" useGrouping={false} />
                                 )} />
                             {getFormErrorMessage("edad")}
-                            <Controller name='sexo' control={control} rules={{ required: 'Los nombres son requeridos' }}
+                            <Controller name='sexo' control={control} rules={{ required: true }}
                                 render={({ field, fieldState }) => (
                                     <Dropdown
                                         optionLabel="label"
-                                        className={fieldState.invalid ? 'md-5 p-invalid' : 'mb-5 md-5'}
+                                        className={fieldState.invalid ? 'md-5 p-invalid mb-2' : 'mb-2 md-5'}
                                         placeholder='Sexo'
                                         options={sexoItems}
                                         value={field.value}
                                         onChange={(e) => field.onChange(e.value)} />
                                 )} />
                             {getFormErrorMessage("sexo")}
-                            <Controller name='peso' control={control} rules={{ required: 'Los nombres son requeridos' }}
+                            <Controller name='peso' control={control} rules={{ required: true }}
                                 render={({ field, fieldState }) => (
-                                    <InputNumber className={fieldState.invalid ? 'md-5 p-invalid' : 'mb-5 md-5'}
+                                    <InputNumber className={fieldState.invalid ? 'md-5 p-invalid mb-2' : 'mb-2 md-5'}
                                         placeholder='Peso' value={field.value}
                                         mode="decimal"
                                         minFractionDigits={1}
@@ -79,9 +84,9 @@ export const Step2 = () => {
                                         onValueChange={(e) => field.onChange(e.value)} suffix=" Kg" />
                                 )} />
                             {getFormErrorMessage("peso")}
-                            <Controller name='altura' control={control} rules={{ required: 'Los nombres son requeridos' }}
+                            <Controller name='altura' control={control} rules={{ required: true }}
                                 render={({ field, fieldState }) => (
-                                    <InputNumber className={fieldState.invalid ? 'md-5 p-invalid' : 'mb-5 md-5'}
+                                    <InputNumber className={fieldState.invalid ? 'md-5 p-invalid mb-2' : 'mb-2 md-5'}
                                         placeholder='Altura'
                                         value={field.value}
                                         minFractionDigits={1}
@@ -90,11 +95,10 @@ export const Step2 = () => {
                                 )} />
                             {getFormErrorMessage("altura")}
                             <div className='flex justify-content-center align-items-center'>
-                                <Button label="Siguiente" type='submit' style={{ background: "#43b06f" }} className="p-button-rounded md:w-4 w-6 p-button-raised justify-content-center align-items-center" />
+                                <Button label="Siguiente" type='submit' style={{ background: "#43b06f" }} className="p-button-rounded md:w-6 w-4 p-button-raised justify-content-center align-items-center" />
                             </div>
                         </form>
-
-                    </Card>
+                    </div>
                 </div>
             </div>
 
--- src/components/recopilacionDatos/step3.jsx
+++ src/components/recopilacionDatos/step3.jsx
@@ -72,27 +72,32 @@ export const Step3 = () => {
 
             <div className='flex'>
                 <img src="/assets/iniciar_sesion_logo.png" alt="Logo de iniciar sesión" className='w-2 absolute left-0 m-4' />
-                <div className=' flex flex-wrap md:justify-content-evenly justify-content-center align-items-center row md:mt-8 mt-7 md:w-11 w-12'>
+                <div className=' flex flex-wrap md:justify-content-evenly justify-content-center align-items-center row md:w-11 w-12 h-screen'>
                     <img className='w-4' src="/assets/Datos_titulo_1.png" alt="Datos" />
-                    <Card className=' md:w-6 w-11' style={{ borderRadius: '5%' }} >
-                        <h2 className='text-center' style={{ color: "#43b06f" }}>Pregunta 2/2</h2>
-                        <form className="flex flex-column p-3 sm:col-12" onSubmit={handleSubmit(goToStep3DataCollection)}>
+                    <div className='w-7 h-full' style={{
+                        background: 'url("/assets/Datos_mano.png")',
+                        backgroundRepeat: 'no-repeat',
+                        backgroundSize: 'cover'
+                    }}>
+                        <form className="flex flex-column p-3 sm:col-2 absolute" style={{ right: "36%" }} onSubmit={handleSubmit(goToStep3DataCollection)}>
+                            <h2 className='text-center' style={{ color: "#43b06f" }}>Pregunta 2/2</h2>
+                            <p className='text-center' >¿padeces alguna de estas enfermedades?</p>
                             <Controller name='enfermedades' control={control} rules={{ required: 'Las enfermedades son requeridas' }}
                                 render={({ field, fieldState }) => (
                                     <MultiSelect
                                         optionLabel="label"
-                                        className={fieldState.invalid ? 'md-5 p-invalid' : 'mb-5 md-5'}
-                                        placeholder='¿Padeces alguna de estas enfermedades?'
+                                        className={fieldState.invalid ? 'md-5 p-invalid' : 'mb-5 text-sm md-5'}
+                                        placeholder='Enfermedades'
                                         options={enfermedadesItems}
                                         value={field.value}
                                         onChange={(e) => field.onChange(e.value)} />
                                 )} />
                             {getFormErrorMessage("enfermedades")}
                             <div className='flex justify-content-center align-items-center'>
-                                <Button label="Finalizar" type='submit' style={{ background: "#43b06f" }} className="p-button-rounded md:w-4 w-6 p-button-raised" />
+                                <Button label="Finalizar" type='submit' style={{ background: "#43b06f" }} className="p-button-rounded md:w-6 w-4 p-button-raised" />
                             </div>
                         </form>
-                    </Card>
+                    </div>
                 </div>
             </div>
 
